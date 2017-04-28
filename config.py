ff33017992562ffe8af11927da92a02d17db2310
Marco Del Tutto <marco.deltutto@physics.ox.ac.uk>
2017-04-28 09:18:11 -0500

    update config

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ PLOT_SUBDIR   = 'plots/'
 BLESSED_PLOTS = './BlessedPlots.json'
 DOCDB_URL     = 'http://microboone-docdb.fnal.gov:8080/cgi-bin/'
 REGENERATE    = False
-PWD           = 'argon!'#open(os.environ['NOVADOCPWDFILE'], 'r').readlines()[0].strip()
+PWD           = open(os.environ['UBDOCPWDFILE'], 'r').readlines()[0].strip()
 
 EXTS          = ['.txt', '.png', '.jpg', '.jpeg', '.gif', '.pdf', '.eps', '.ps', '.C', '.tar.gz', '.zip']
